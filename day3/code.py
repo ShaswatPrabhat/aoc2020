411d7645e40d93dc7bf736a45f6965dc08605e4d
Shaswat Prabhat <spd1238@gmail.com>
2020-12-05 10:15:05 +0530

    Day3 and Day 4 solutions.

--- day3/code.py
+++ day3/code.py
@@ -1,26 +1,37 @@
-def is_valid(line):
-    split_col = line.split(':')
-    password = split_col[1].strip()
-    test_clause = split_col[0]
-    number_clause = test_clause.split(' ')[0]
-    character_clause = test_clause.split(' ')[1]
-    min = int(number_clause.split('-')[0]) - 1
-    max = int(number_clause.split('-')[1]) - 1
-    if password[min] == character_clause and password[max] != character_clause:
-        return True
-    elif password[max] == character_clause and password[min] != character_clause:
-        return True
-    else:
-        return False
+def treecount(paths, slope):
+    if slope[1] > 1:
+        paths = paths[::slope[1]]
+        print(paths)
+    width = len(paths[0])
+    x = 0
+    y = 0
+    height = len(paths)
+    count = 0
+    for i in range(height - 1):
+        x += slope[0]
+        x = x % width
+        y += 1
+        if paths[y][x] == "#":
+            count += 1
+    return count
 
-def get_valid_sets(array_of_input):
-    counter = 0
-    for line in array_of_input:
-        if is_valid(line):
-            counter += 1
-    return counter
+
+def get_number_of_trees(array):
+    return treecount(array, [3, 1])
+
+
+def create_array_from_input(readLines):
+    matrix = []
+    for w in readLines:
+        a = []
+        for character in w.strip():
+            a.append(character)
+        matrix.append(a)
+    return matrix
 
 
 if __name__ == '__main__':
-    f = open('input_real.txt', 'r')
-    print(get_valid_sets(f.readlines()))
+    f = open('input_test.txt', 'r')
+
+    print(get_number_of_trees(create_array_from_input(f.readlines())))
+    f.close()
