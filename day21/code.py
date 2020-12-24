58a15e3666e0661ae6648bfae019f93c528c728b
Shaswat Prabhat <spd1238@gmail.com>
2020-12-24 09:50:33 +0530

    Solutions for day 21,22,23.

---
+++ day21/code.py
@@ -0,0 +1,92 @@
+import re
+
+
+def parse_foods(s):
+    foods = []
+    for line in s.splitlines():
+        m = re.fullmatch('([a-zA-Z ]+) \(contains ([a-zA-Z ,]+)\)', line)
+        ingredients = set(m.group(1).split())
+        allergens = set(m.group(2).split(', '))
+        foods.append((ingredients, allergens))
+    return foods
+
+
+def part1(s):
+    foods = parse_foods(s)
+
+    all_ingredients = set()
+    all_allergens = set()
+
+    for ingredients, allergens in foods:
+        all_ingredients |= ingredients
+        all_allergens |= allergens
+
+    possible_allergens = set()
+
+    for allergen in all_allergens:
+        possibilities = set(all_ingredients)
+        for ingredients, allergens in foods:
+            if allergen in allergens:
+                possibilities &= ingredients
+        possible_allergens |= possibilities
+
+    safe_ingredients = all_ingredients - possible_allergens
+
+    answer = 0
+    for ingredients, allergens in foods:
+        answer += len(ingredients & safe_ingredients)
+    print(f'one {answer}')
+
+
+def part2(s):
+    foods = parse_foods(s)
+
+    all_ingredients = set()
+    all_allergens = set()
+
+    for ingredients, allergens in foods:
+        all_ingredients |= ingredients
+        all_allergens |= allergens
+
+    possible_bad_ingredients = set()
+
+    for allergen in all_allergens:
+        possibilities = set(all_ingredients)
+        for ingredients, allergens in foods:
+            if allergen in allergens:
+                possibilities &= ingredients
+        possible_bad_ingredients |= possibilities
+
+    assert (len(possible_bad_ingredients) == len(all_allergens))
+
+    allergen_to_triggers = {}
+    for allergen in all_allergens:
+        allergen_to_triggers[allergen] = set(possible_bad_ingredients)
+
+    for ingredients, allergens in foods:
+        for allergen in allergens:
+            allergen_to_triggers[allergen] &= ingredients
+
+    def solved():
+        for zuzu in allergen_to_triggers.values():
+            if len(zuzu) > 1:
+                return False
+        return True
+
+    while not solved():
+        identified = set()
+        for thing in allergen_to_triggers.values():
+            if len(thing) == 1:
+                identified |= thing
+        for allergen, thing in allergen_to_triggers.items():
+            if len(thing) > 1:
+                allergen_to_triggers[allergen] -= identified
+
+    stuff = sorted(allergen_to_triggers.items())
+    answer = ','.join(list(trigger)[0] for _, trigger in stuff)
+
+    print(f'two {answer}')
+
+
+part1(open('input_real.txt', 'r').read())
+part2(open('input_real.txt', 'r').read())
---
+++ day23/code.py
@@ -0,0 +1,110 @@
+from collections import deque
+
+
+class MyLLNode:
+
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+def part1(input):
+    cups = deque(input)
+
+    for _ in range(100):
+        orig_val = cups[0]
+        dest_val = cups[0] - 1
+        if dest_val < 1:
+            dest_val += 9
+        cups.rotate(-1)
+
+        c1 = cups.popleft()
+        c2 = cups.popleft()
+        c3 = cups.popleft()
+
+        while dest_val in (c1, c2, c3):
+            dest_val = dest_val - 1 if dest_val > 1 else dest_val + 8
+
+        while cups[0] != dest_val:
+            cups.rotate(-1)
+        cups.rotate(-1)
+
+        cups.append(c1)
+        cups.append(c2)
+        cups.append(c3)
+
+        while cups[0] != orig_val:
+            cups.rotate(-1)
+        cups.rotate(-1)
+
+    while cups[0] != 1:
+        cups.rotate(-1)
+    cups.popleft()
+
+    return ''.join([str(i) for i in cups])
+
+
+def part2(input):
+    nodes = {}
+
+    # Build from input
+    last_node = None
+    for i in input:
+        node = MyLLNode(i)
+        nodes[i] = node
+        if last_node is not None:
+            last_node.right = node
+            node.left = last_node
+        last_node = node
+
+    # Complete 1 million nodes
+    for i in range(len(input) + 1, 1_000_001):
+        node = MyLLNode(i)
+        nodes[i] = node
+        if last_node is not None:
+            last_node.right = node
+            node.left = last_node
+        last_node = node
+
+    # Complete the circle
+    ptr = nodes[input[0]]
+    last_node.right = ptr
+    ptr.left = last_node
+
+    assert len(nodes) == 1_000_000
+
+    ptr = nodes[input[0]]
+    for i in range(10_000_000):
+        # if i % 500_000 == 0:
+        #     print(i)
+        p_val = ptr.val
+
+        c1 = ptr.right
+        c2 = c1.right
+        c3 = c2.right
+
+        ptr.right = c3.right
+        ptr.right.left = ptr
+
+        d_val = p_val - 1 or 1_000_000
+        while d_val in (c1.val, c2.val, c3.val):
+            d_val = d_val - 1 or 1_000_000
+
+        d_node = nodes[d_val]
+
+        c3.right = d_node.right
+        c3.right.left = c3
+        d_node.right = c1
+        c1.left = d_node
+
+        ptr = ptr.right
+
+    while ptr.val != 1:
+        ptr = ptr.right
+
+    return ptr.right.val * ptr.right.right.val
+
+
+if __name__ == '__main__':
+    print(part2([int(a) for a in open('input_real.txt').read()]))
